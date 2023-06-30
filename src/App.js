2c41d8b51387af69c2374e1afeba3920da9c52c4
Rohit Saini <rs0118032@gmail.com>
2023-06-30 14:59:51 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+// import React from "react";
+import { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const options = [
+    // { label:'Select',value:'Select'},
+    { key:1, label: 'Yes', value: 'Yes' },
+    { key:2, label: 'Probably Not', value: 'No' }
+  ];
+ 
+  const [value, setValue] = useState('fruit');
+ 
+  const handleChange = (event) => {     
+    setValue(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div id="drpdwn-cont"> 
+
+          <label >
+            Should You use a Dropdown? 
+          </label>
+
+            <select value={value} onChange={handleChange} name="select" placeholder="Select">
+              {options.map((option) => (
+                <option key={option.key} value={option.value}>{option.label}</option>
+              ))}
+            </select>
+          
+      </div>
     </div>
   );
 }
