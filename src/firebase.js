eddd3fc63bbb78166433fc81f28de46b5c6864f0
Rohit Saini <rs0118032@gmail.com>
2023-06-30 16:05:03 +0530

    final changes

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJGdRSZo2BhN0qqpmNVmh53bcp7WI9xQk",
+  authDomain: "dropdown-4d9cd.firebaseapp.com",
+  projectId: "dropdown-4d9cd",
+  storageBucket: "dropdown-4d9cd.appspot.com",
+  messagingSenderId: "570016229800",
+  appId: "1:570016229800:web:8e8a02ce33dcbcae0a159d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
